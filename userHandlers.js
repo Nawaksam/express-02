f71fd0309a61ac9a34e46bc00eba5d420969657f
Nawaksam <sam.cardonnel@gmail.com>
2023-07-24 10:45:59 +0200

    quest-04

--- userHandlers.js
+++ userHandlers.js
@@ -43,8 +43,32 @@ const postUser = (req, res) => {
       res.status(500).send("Error saving the user")
     })
 }
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id, 10)
+  const { firstname, lastname, email, city, language } = req.body
+
+  database
+    .query(
+      "UPDATE `users` SET `firstname` = ? , `lastname` = ?, `email` = ?, `city` = ?, `language` = ? WHERE `id` = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404)
+      } else {
+        res.sendStatus(200)
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+      res.sendStatus(500)
+    })
+}
+
 module.exports = {
   getUsers,
   getUserById,
   postUser,
+  updateUser,
 }
